404bb3b9315d12f16b58dc388d72aa40b8bc5fcf
mazhongquan <mazhongquan>
2019-12-02 17:19:03 +0800

    提交代码 dict

---
+++ src/main/java/com/dict/mark/control/ControlTest.java
@@ -0,0 +1,14 @@
+package com.dict.mark.control;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class ControlTest {
+    @RequestMapping
+    public String test(){
+        return "hello word";
+    }
+
+}
