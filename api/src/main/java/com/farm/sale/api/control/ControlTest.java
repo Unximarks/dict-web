bb2917eb14b301616abfdf77ea548f6c38f99533
mazhongquan < @mazhongquan>
2020-07-08 14:32:53 +0800

    redis 配置  键值序列化

--- api/src/main/java/com/farm/sale/api/control/ControlTest.java
+++ api/src/main/java/com/farm/sale/api/control/ControlTest.java
@@ -3,6 +3,7 @@ package com.farm.sale.api.control;
 
 import com.farm.sale.service.api.AgenceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +13,20 @@ import java.util.List;
 @RestController
 public class ControlTest {
     @Autowired
+    RedisTemplate redisTemplate;
+    @Autowired
     private AgenceService agenceService;
 
     @RequestMapping
     public String test() {
+        redisTemplate.opsForValue().set("marks1","嘻嘻");
+        String value = (String) redisTemplate.opsForValue().get("marks1");
         List<String> strings = agenceService.selectAgence();
         String string = "";
         for (String s : strings) {
             string = string + s;
         }
-        return "test " + string;
+        return "test " + string + "redis:" + value;
     }
 
 }
